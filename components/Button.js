98400ba90b4edae39dc597d7d7b71499c2cbe4a0
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-06-15 15:13:42 -0400

    Create landing page

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from './button.module.css'
 
 export const MyButton = (props) => {
   const { name } = props;
   return (
     <div>
-      <button className="">{name}</button>
+      <button className={styles.text  }>{name}</button>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
 import { SignInd } from '../components/SignInd'
-import { MyButton } from '../components/Button'
 import styles from '../styles/index.module.css'
 
 export default function Home() {
   return (
     <>
-    <div className={styles.container}>
     <SignInd/>
-     <MyButton name="Enviar"/>
-    </div>
-
     </>
 
 
